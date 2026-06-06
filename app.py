bb804e2be901d317ffcd37b9d9f95fe943610ac5
Cokegirl81 <139410226+Cokegirl81@users.noreply.github.com>
2026-06-06 09:49:25 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,124 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="EcoCondomínio",
+    page_icon="♻️",
+    layout="wide"
+)
+
+st.sidebar.title("♻️ EcoCondomínio")
+
+pagina = st.sidebar.radio(
+    "Menu",
+    [
+        "Início",
+        "O que reciclar",
+        "Calendário",
+        "Dicas",
+	"Indicadores"
+    ]
+)
+
+if pagina == "Início":
+
+    st.title("♻️ EcoCondomínio")
+
+    st.markdown("""
+    ### Sustentabilidade ao alcance de todos
+
+    Aplicativo criado para auxiliar moradores de condomínios
+    na separação correta dos resíduos e incentivar práticas sustentáveis.
+    """)
+
+    st.success(
+        "Pequenas ações geram grandes impactos ambientais."
+    )
+
+elif pagina == "O que reciclar":
+
+    st.title("♻️ O Que Reciclar")
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.info("📄 Papel")
+        st.write("""
+        • Jornais  
+        • Revistas  
+        • Caixas de papelão
+        """)
+
+        st.info("🥤 Plástico")
+        st.write("""
+        • Garrafas PET  
+        • Embalagens  
+        • Potes
+        """)
+
+    with col2:
+        st.info("🍾 Vidro")
+        st.write("""
+        • Garrafas  
+        • Frascos
+        """)
+
+        st.info("🥫 Metal")
+        st.write("""
+        • Latas  
+        • Tampas metálicas
+        """)
+
+elif pagina == "Calendário":
+
+    st.title("🗑️ Calendário de Coleta")
+
+    st.write("**Segunda:** Papel")
+    st.write("**Quarta:** Plástico")
+    st.write("**Sexta:** Vidro e Metal")
+
+    st.success(
+        "Consulte regularmente os dias de coleta seletiva."
+    )
+
+elif pagina == "Indicadores":
+
+    st.title("📊 Indicadores da Pesquisa")
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.metric(
+            "Moradores entrevistados",
+            10
+        )
+
+    with col2:
+        st.metric(
+            "Conheciam reciclagem",
+            "40%"
+        )
+
+    with col3:
+        st.metric(
+            "Acharam o app útil",
+            "90%"
+        )
+
+    st.write("Resultados obtidos durante a validação da proposta.")
+
+elif pagina == "Dicas":
+    st.title("🌱 Dicas Sustentáveis")
+
+    st.markdown("""
+    ✅ Reutilize embalagens
+
+    ✅ Evite desperdício de água
+
+    ✅ Separe corretamente os resíduos
+
+    ✅ Prefira produtos recicláveis
+
+    ✅ Incentive sua família a reciclar
+    """)
+
+    st.balloons()
